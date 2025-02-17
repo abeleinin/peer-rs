907a50af7f2ad3fe4f1ad465584190240ca1fd4a
aleinin <95333017+abeleinin@users.noreply.github.com>
2025-02-16 23:54:06 -0600

    webrtc api peer connection

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,22 @@ use env_logger;
 use log::{info};
 use anyhow::Result;
 
+use webrtc::api::APIBuilder;
+use webrtc::peer_connection::configuration::RTCConfiguration;
+
 #[tokio::main]
 async fn main() -> Result<()> {
     env_logger::init();
+
+    let api = APIBuilder::new().build();
+
+    let config = RTCConfiguration {
+        ..Default::default()
+    };
+
+    let peer_connection = api.new_peer_connection(config).await?;
 
     info!("Starting WebRTC peer-to-peer audio connection...");
     
     Ok(())
 }
-
